0062fe6528e18ddc5d8ebb2c94b02fa6198a02cf
Myltiane <myltiane.aux@gmail.com>
2023-03-04 10:18:10 -0300

    feat: iniciando aula 03

--- classes/class03/pages/titles/src/views/handGestureView.js
+++ classes/class03/pages/titles/src/views/handGestureView.js
@@ -1,4 +1,13 @@
 export default class HandGestureView {
+    #handsCanvas = document.querySelector('#hands')
+    #canvasContext = this.#handsCanvas.getContext('2d')
+    #fingerLookupIndexes
+    constructor({ fingerLookupIndexes}) {
+        this.#handsCanvas.width = globalThis.screen.availWidth
+        this.#handsCanvas.height = globalThis.screen.availHeight
+        this.#fingerLookupIndexes = fingerLookupIndexes
+    }
+
     loop(fn) {
         requestAnimationFrame(fn)
     }
